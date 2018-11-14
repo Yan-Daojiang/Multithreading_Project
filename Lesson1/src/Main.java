dbc6ad77cd699fe20ce0abd5dc30b850b0092ffc
颜道江· <1373836603@qq.com>
2018-11-14 19:44:29 +0800

    finish Lesson1

--- Lesson1/src/Main.java
+++ Lesson1/src/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 /**
  * @author: Yan_Daojiang
  * @date: 2018/11/13
- * @description:
+ * @description:程序的主类，负责登录的相关的信息
  **/
 import java.util.*;
 import java.io.*;
@@ -17,32 +17,38 @@ public class Main {
             //对菜单选项进行输出
             Scanner sc = new Scanner(System.in);
             System.out.println("1.登录\n2.退出");
-            System.out.printf("请输入您的选择：");
-            in = sc.next();
+            System.out.printf("请输入您的选择:");
+
             //根据输入进行相关的操作
+            in = sc.next();
             switch (in) {
                 case "1": {//执行登录的相关操作
-                    String nothing=sc.nextLine();
+                    //从键盘输入登录人员的用户名和密码
+                    sc.nextLine();
                     System.out.print("请输入您的姓名:");
-                    //登录界面的相关信息的输入
                     String name=sc.nextLine();
-                    System.out.print("请输入您的密码：");
+                    System.out.print("请输入您的密码:");
                     String pwd=sc.nextLine();
+
                     //在系统中进行查找
                     //如果找到相关的用户，就执行后面的操作；否则提示用户检查输入的信息
                     User temp=DataProcessing.search(name,pwd);
+
                     if(temp!=null)
                         //找到就根据身份进行对象的创建
+                        //根据temp的身份显示相关管理人员的菜单信息
                        temp.showMenu();
                     else
                         System.out.println("请检查您的输入是否有误！");
                     break;
                 }
+
                 case "2": {//退出系统
                     System.out.println("系统退出, 谢谢使用 ! ");
                     System.exit(0);
                 }
-                default: {
+
+                default: {//对密码错误等情况给出提示
                     System.out.println("您的输入有误，请检查你的输入 ");
                     break;
                 }
--- Lesson1/src/Operator.java
+++ Lesson1/src/Operator.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 /**
  * @author: Yan_Daojiang
  * @date: 2018/11/13
- * @description:
+ * @description:操作人员类，主要负责操作人员的相关的操作
  **/
 public class Operator extends User {
     public Operator(String name, String password, String role) {
@@ -12,14 +12,11 @@ public class Operator extends User {
     }
 
     //上传文件
-    public void updatefile(){
-        System.out.println("请输入文件名称：");
-        Scanner scanner=new Scanner(System.in);
-        String nothing=scanner.nextLine();
-        String file=scanner.nextLine();
+    public void updatefile(String filename,String num,String description){
         System.out.println("上传文件·····");
     }
 
+    //显示档案录入人员菜单
     public void showMenu(){
         System.out.println("************您已进入档案录入人员系统*****************");
         String in="abc";
@@ -27,19 +24,30 @@ public class Operator extends User {
             //对菜单选项进行输出
             Scanner sc = new Scanner(System.in);
             System.out.println("1.上传文件\n2.下载文件\n3.文件列表\n4.修改密码\n5.退出\n");
-            System.out.printf("请输入您的选择：");
-            in = sc.next();
+
             //根据输入进行相关的操作
+            System.out.printf("请输入您的选择:");
+            in = sc.next();
             switch (in) {
                 case "1":{//上传文件
-                    updatefile();
+                    sc.nextLine();
+                    System.out.print("请输入文件名称:");
+                    String filename=sc.nextLine();
+                    System.out.print("请输入档案编号:");
+                    String num=sc.nextLine();
+                    System.out.print("请输入档案描述:");
+                    String description=sc.nextLine();
+                    updatefile(filename,num,description);
                     break;
                 }
                 case "2": {//下载文件
-                    String nothing=sc.nextLine();
-                    System.out.println("请输入您要下载的文件的名称:");
+                    sc.nextLine();
+                    System.out.print("请输入要下载的文件名:");
                     String filename=sc.nextLine();
-                    downloadFile(filename);
+                    if(downloadFile(filename))
+                        System.out.println("文件下载成功\n");
+                    else
+                        System.out.println("文件下载失败\n");
                     break;
                 }
                 case "3": {//文件列表的显示
@@ -47,10 +55,16 @@ public class Operator extends User {
                     break;
                 }
                 case "4":{//修改密码
-
+                    sc.nextLine();
+                    System.out.print("请输入你的新密码:");
+                    String pwd = sc.nextLine();
+                    if (changeSelfInfo(pwd))
+                        System.out.println("密码修改成功!");
+                    else
+                        System.out.println("密码修改不成功!");
                     break;
                 }
-                case "5":{
+                case "5":{//退出系统
                     exitSystem();
                 }
                 default: {
